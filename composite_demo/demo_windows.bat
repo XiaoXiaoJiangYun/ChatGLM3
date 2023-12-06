aa5bec5d6e8f19721913d4c20d1e91d63b98b164
XiaoXiaoJiangYun <33343036+XiaoXiaoJiangYun@users.noreply.github.com>
2023-12-06 11:21:42 +0800

    Add windows demo
    Add windows demo that support PRE_SEQ_LEN&MAX_LENGTH

---
+++ composite_demo/demo_windows.bat
@@ -0,0 +1,7 @@
+set MODEL_PATH=E:\\chatglm3-6b
+set PT_PATH=D:\\ChatGLM3\\finetune_chatmodel_demo\\output\\tool_alpaca_pt-20231205-160808-512-2e-2\\checkpoint-1000
+set PRE_SEQ_LEN=512
+set MAX_LENGTH=20000
+streamlit run main.py
+
+pause
